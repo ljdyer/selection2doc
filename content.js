cf03420b64a7e57045a60c9d869cf0501d3fd8d4
ljdyer <74581995+ljdyer@users.noreply.github.com>
2021-06-26 07:39:53 +0100

    Ver 1.0.0.3
    Reflected line breaks
    Changed file name format
    Tidied up code

--- content.js
+++ content.js
@@ -9,28 +9,51 @@ chrome.runtime.onMessage.addListener(
 );
 
 function getAllText(storageItems){
-  list = storageItems.idList;
-  var elementIds = list.split(",");
-  console.log(elementIds);
-  elementText = elementIds.map(getElementText);
-  console.log(elementText);
-  makeDoc(elementText);
+  let list = storageItems.idList;
+
+  let elementIds = splitAndTrim(list);
+  let elementText = elementIds.map(getElementText);
+  let elementTextWithBlankLines = intersperse(elementText, "");
+  let lines = elementTextWithBlankLines.map(splitByLineBreak).flat();
+
+  console.log(lines);
+  makeDoc(lines);
 }
 
 function getElementText(elementId){
   let elementText = $(`#${elementId}`).text();
-  console.log("====================");
-  console.log(`ID: ${elementId}`);
-  console.log($(`#${elementId}`).text());
-  console.log("====================");
+  // console.log("====================");
+  // console.log(`ID: ${elementId}`);
+  // console.log($(`#${elementId}`).text());
+  // console.log("====================");
   return elementText;
 }
 
+function splitAndTrim(commaList){
+  result = commaList.split(",").map(trimString);
+  return result;
+}
+
+function splitByLineBreak(text){
+  let result = text.split("\n");
+  return result;
+}
+
+function intersperse(array, seperator){
+  console.log(array);
+  result = array.flatMap(e => [seperator,e]).slice(1);
+  return result;
+}
+
+function trimString(string){
+  return string.trim();
+}
 
 
 
 function makeDoc(allText){
   let paragraphs = allText.map(paragraphFromText);
+  let charCount = countChars(allText);
 
   const doc = new docx.Document({
     sections: [
@@ -43,7 +66,8 @@ function makeDoc(allText){
 
   docx.Packer.toBlob(doc).then((blob) => {
     console.log(blob);
-    saveAs(blob, "example.docx");
+    let filename = generateFilename(charCount);
+    saveAs(blob, filename);
     console.log("Document created successfully");
   });
 }
@@ -51,8 +75,42 @@ function makeDoc(allText){
 function paragraphFromText(text){
   result = new docx.Paragraph({
     children: [
-      new docx.TextRun({ text: text, break: 1 }),
+      new docx.TextRun({ text: text, break: 0 }),
     ]
   });
   return result;
 }
+
+function countChars(text){
+  let charCounts = text.map(removeSpaces).map(getStringLength);
+  // console.log(charCounts);
+  let totalCharCount = charCounts.reduce((a, b) => a + b, 0);
+  return totalCharCount;
+}
+
+function removeSpaces(string){
+  return string.replace(" ", "");
+}
+
+function getStringLength(string){
+  return string.length;
+}
+
+function generateFilename(charCount){
+  let filename = `${document.title} ${getFormattedTime()} (${charCount} characters).docx`;
+  return filename;
+}
+
+function getFormattedTime(){
+  var today = new Date();
+  var y = today.getFullYear();
+  // JavaScript months are 0-based.
+  var m = today.getMonth() + 1;
+  var d = today.getDate();
+  var h = today.getHours();
+  var mi = today.getMinutes();
+  var s = today.getSeconds();
+  return y + "-" + m + "-" + d + " " + h + "-" + mi + "-" + s;
+}
+
+
